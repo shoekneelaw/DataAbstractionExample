a61ddf399df7b65346dcf632faa4c78a23429f0c
shoekneelaw <shoekneelaw@gmail.com>
2016-12-02 07:51:40 +0545

    commited

---
+++ shoot/ShotGun.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.leapfrog.shoot;
+
+/**
+ *
+ * @author user
+ */
+public class ShotGun extends Gun{
+
+    @Override
+    public void kills() {
+        System.out.println("And The Gun you are using is shotgun.");
+    }
+    
+    
+}
